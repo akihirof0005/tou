10fe8793f66f576e0535c54f0e0c96ab4cc4bdd7
akihiro.F <akihirof0005@gmail.com>
2021-01-11 10:12:38 +0900

    update

--- src/main.rs
+++ src/main.rs
@@ -6,19 +6,35 @@ mod manipulation;
 
 mod structs;
 
-use std::env;
+//use std::env;
+use std::collections::HashMap;
+use std::fs;
+
+use rustc_serialize::json;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let filename = &args[1];
-    let tou: Vec<structs::Atom> = manipulation::tou_reader(filename.to_string());
+    let paths = fs::read_dir("./pdb").unwrap();
+    let mut files: Vec<String> = vec![];
+    for path in paths {
+        files.push(path.unwrap().path().display().to_string());
+    }
 
-    let oh_count = finding_partial_structures::is_included_oh(&tou);
-    let cooh_count = finding_partial_structures::is_included_cooh(&tou);
-    let noc_count = finding_partial_structures::is_included_noc(&tou);
-    let nh2_count = finding_partial_structures::is_included_nh2(&tou);
-    println!(
-        "OH:{:?} COOH:{:?} NOC:{:?} NH2:{:?}",
-        oh_count, cooh_count, noc_count, nh2_count
-    );
+    //   let args: Vec<String> = env::args().collect();
+    let mut result: Vec<HashMap<&str, i32>> = vec![];
+    for file in files.iter() {
+        let filename = &file;
+        let tou: Vec<structs::Atom> = manipulation::tou_reader(filename.to_string());
+
+        let oh_count: i32 = finding_partial_structures::is_included_oh(&tou);
+        let cooh_count: i32 = finding_partial_structures::is_included_cooh(&tou);
+        let noc_count: i32 = finding_partial_structures::is_included_noc(&tou);
+        let nh2_count: i32 = finding_partial_structures::is_included_nh2(&tou);
+        let mut tou: HashMap<&str, i32> = HashMap::new();
+        tou.insert("OH", oh_count);
+        tou.insert("COOH", cooh_count);
+        tou.insert("NOC", noc_count);
+        tou.insert("NH2", nh2_count);
+        result.push(tou);
+    }
+    println!("{}", json::encode(&result).unwrap());
 }
